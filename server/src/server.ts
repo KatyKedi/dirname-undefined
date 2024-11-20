7f85689c158f5c5b3cef53bff2fa6806e1d297f4
Katy <katykat5bros@gmail.com>
2024-11-20 09:03:42 -0700

    define dirname

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,6 +13,9 @@ import { authenticateToken } from './services/auth-service.js';
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
